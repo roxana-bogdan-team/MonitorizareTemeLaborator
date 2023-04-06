127835eb998e6bc3bf8b4e4c52b26c07ba1cc01b
Zachman Roxana <zrie3047@scs.ubbcluj.ro>
2023-04-06 19:56:39 +0300

    Added more tests + fixed bug

--- src/main/java/ssvv/project/repository/AbstractCRUDRepository.java
+++ src/main/java/ssvv/project/repository/AbstractCRUDRepository.java
@@ -37,7 +37,7 @@ public abstract class AbstractCRUDRepository<ID, E extends HasID<ID>> implements
             return entities.putIfAbsent(entity.getID(), entity);
         } catch (ValidationException ve) {
             System.out.println("Entitatea nu este valida! \n");
-            return null;
+            return entity;
         }
     }
 
--- src/test/java/ssvv/project/repository/StudentRepositoryTest.java
+++ src/test/java/ssvv/project/repository/StudentRepositoryTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 import ssvv.project.domain.Student;
 import ssvv.project.validation.StudentValidator;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class StudentRepositoryTest {
 
@@ -18,10 +17,10 @@ public class StudentRepositoryTest {
     }
 
     @Test
-    public void testSaveInvalidStudent() {
-        final String id = "777";
-        final String nume = "nume1";
-        final int grupa = 939;
+    public void testSaveValidStudent() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 937;
         Student student = new Student(id, nume, grupa);
 
         final Student result = studentRepository.save(student);
@@ -30,12 +29,120 @@ public class StudentRepositoryTest {
     }
 
     @Test
-    public void testSaveValidStudent() {
-        final String id = "777";
+    public void testSaveStudentNullId() {
+        final String id = null;
         final String nume = "nume1";
         final int grupa = 937;
         Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentEmptyId() {
+        final String id = "";
+        final String nume = "nume1";
+        final int grupa = 937;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentNullName() {
+        final String id = "1";
+        final String nume = null;
+        final int grupa = 937;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentEmptyName() {
+        final String id = "1";
+        final String nume = "";
+        final int grupa = 937;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentGroup110() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 110;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveExistingStudent() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 937;
+        Student student = new Student(id, nume, grupa);
         studentRepository.save(student);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentGroup109() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 109;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentGroup111() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 111;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testSaveStudentGroup938() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 938;
+        Student student = new Student(id, nume, grupa);
+
+        final Student result = studentRepository.save(student);
+
+        assertEquals(student, result);
+    }
+
+    @Test
+    public void testSaveStudentGroup939() {
+        final String id = "1";
+        final String nume = "name";
+        final int grupa = 939;
+        Student student = new Student(id, nume, grupa);
 
         final Student result = studentRepository.save(student);
 
